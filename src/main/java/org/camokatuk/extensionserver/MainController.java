91c4319e4411b7b2f113a1b1ab0923b11b693cfa
camokatuk <black.valuable.notebook@gmail.com>
2019-07-14 22:42:14 +0200

    added auth stub

--- src/main/java/org/camokatuk/extensionserver/MainController.java
+++ src/main/java/org/camokatuk/extensionserver/MainController.java
@@ -1,5 +1,6 @@
 package org.camokatuk.extensionserver;
 
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,4 +23,34 @@ public class MainController
 		}
 	}
 
+	@RequestMapping("/auth")
+	public String acceptJwt(@RequestParam("uid") String userId, @RequestHeader("x-extension-jwt") String jwt)
+	{
+		System.out.println("JWT: " + jwt);
+		return jwt;
+	}
+
 }
+/*// create the request options for our Twitch API calls
+const requests = {
+  checkuserid: userlookup()
+};
+
+function userlookup () {
+  return {
+    type: 'GET',
+    url:'https://api.twitch.tv/helix/users?display_name=ahorseinahospital',
+    success: logSuccess,
+    error: logError,
+	headers: { 'Client-ID': 'wj2tk1o40kcqsekdv2iknadal1krac' }
+  };
+}
+
+function setAuth (token) {
+  Object.keys(requests).forEach((req) => {
+    twitch.rig.log('Setting auth headers');
+    requests[req].headers = { 'Authorization': 'Bearer ' + token };
+  });
+}
+
+*/
