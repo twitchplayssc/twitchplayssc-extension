2d41e63331b120de8d51734800b5437f1fa2126d
camokatuk <black.valuable.notebook@gmail.com>
2019-07-15 18:17:22 +0200

    zoom blind test

--- src/main/resources/static/viewer.js
+++ src/main/resources/static/viewer.js
@@ -70,7 +70,7 @@ $.fn.extend({ // avoids triggering show animation when not necessary
 $(function () {
 //	toggleMode(true);
 	//$('.feeding').toggle(true);
-	$('body').css('background-image', 'url("img/bg.png")');
+//	$('body').css('background-image', 'url("img/bg.png")');
 	pollResourcesPeriodically();
 });
 
