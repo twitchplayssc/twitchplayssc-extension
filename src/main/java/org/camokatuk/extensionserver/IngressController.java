8c8426e6964560d8afa1794cbdc2f346b733b321
camokatuk <black.valuable.notebook@gmail.com>
2019-07-21 20:46:45 +0200

    Im an idiot, rolled back IngressController

--- src/main/java/org/camokatuk/extensionserver/IngressController.java
+++ src/main/java/org/camokatuk/extensionserver/IngressController.java
@@ -1,10 +1,7 @@
 package org.camokatuk.extensionserver;
 
-import java.io.IOException;
 import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,14 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @RestController
 @RequestMapping("/api")
 public class IngressController
 {
-	private static final Log LOG = LogFactory.getLog(IngressController.class);
-
 	private final StateManager stateManager;
 	private final String superSecretKey;
 
@@ -37,17 +30,13 @@ public class IngressController
 	@PostMapping("/playerstats")
 	public
 	@ResponseBody
-	String pushStats(@RequestBody String request, @RequestHeader(value = "Authentication") String ohWowSecurity) throws IOException
+	String pushStats(@RequestBody Map<String, UserDisplayData> state, @RequestHeader(value = "Authentication") String ohWowSecurity)
 	{
 		if (isNotAuthorizedRequest(ohWowSecurity))
 		{
 			return "Nope";
 		}
 
-		LOG.info("Raw request " + request);
-
-		ObjectMapper mapper = new ObjectMapper();
-		Map<String, UserDisplayData> state = mapper.readValue(request, Map.class);
 		stateManager.pushPlayerStats(state);
 		return "OK";
 	}
