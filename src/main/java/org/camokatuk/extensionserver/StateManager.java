dd9f5a175a5573ce7e084a4bfb229105d1d403ea
camokatuk <black.valuable.notebook@gmail.com>
2020-03-17 02:34:41 +0100

    refactoring + backend for events

--- src/main/java/org/camokatuk/extensionserver/StateManager.java
+++ src/main/java/org/camokatuk/extensionserver/StateManager.java
@@ -1,5 +1,6 @@
 package org.camokatuk.extensionserver;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -9,70 +10,55 @@ import org.camokatuk.extensionserver.twitchapi.TwitchApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import static org.camokatuk.extensionserver.AutoIdUserDataContainer.parseNumericUserId;
+
 @Component
 public class StateManager
 {
 	private static final Log LOG = LogFactory.getLog(StateManager.class);
 
-	private static final String UNAME_FLAG_PREFIX = "uname_";
-
-	private final TwitchApi twitchApi;
-	private volatile Map<Integer, UserDisplayData> statsByUserId;
-	private volatile Map<String, UserDisplayData> statsByUserName; // fallback, will be eliminated
-
-	private volatile Map<String, Integer> displayNameToUid = new ConcurrentHashMap<>();
+	private final AutoIdUserDataContainer<UserGameState> resources;
+	private final AutoIdUserDataContainer<List<String>> events;
 
 	private volatile GameStateContainer gameState = new GameStateContainer();
 
 	@Autowired
 	public StateManager(TwitchApi twitchApi)
 	{
-		this.twitchApi = twitchApi;
+		Map<String, Integer> displayNameToUid = new ConcurrentHashMap<>();
+		this.resources = new AutoIdUserDataContainer<>(twitchApi, displayNameToUid);
+		this.events = new AutoIdUserDataContainer<>(twitchApi, displayNameToUid);
 		this.resetPlayerStats();
 	}
 
 	private void resetPlayerStats()
 	{
-		this.statsByUserId = new ConcurrentHashMap<>();
-		this.statsByUserName = new ConcurrentHashMap<>();
+		this.resources.reset();
+		this.events.reset();
 	}
 
-	public void pushPlayerStats(Map<String, UserDisplayData> state)
+	public void pushResources(Map<String, UserGameState> state)
 	{
+		if (state == null)
+		{
+			LOG.warn("Got null state map, what the hell");
+			return;
+		}
+
 		// if the extension server was just restarted or somehow we didn't get the initial push from the bot
 		// and yet we're receiving players' data, we should update the global state to INGAME.
 		// it could be a sellout game, which would imply INGAME_SELLOUT, but the chances of that are low,
 		// and we shouldn't be restarting the extension server during the sellout games in the first place.
 		// we can always fix this by pushing INGAME_SELLOUT manually.
 		boolean stateIsNotInGame = this.gameState == null || GameState.isNotInGame(this.gameState.getState());
-		if (state != null && !state.isEmpty() && stateIsNotInGame)
+		if (!state.isEmpty() && stateIsNotInGame)
 		{
 			this.gameState = GameStateContainer.inGame();
 		}
 
-		for (Map.Entry<String, UserDisplayData> stateEntry : state.entrySet())
+		for (Map.Entry<String, UserGameState> stateEntry : state.entrySet())
 		{
-			String userKey = stateEntry.getKey();
-			Integer userId;
-			String userName = null;
-			if (userKey.startsWith(UNAME_FLAG_PREFIX))
-			{
-				userName = userKey.substring(UNAME_FLAG_PREFIX.length()).toLowerCase();
-				userId = displayNameToUid.get(userName); // can be null, since displayNameToUid is populated when user requests their stats
-			}
-			else
-			{
-				userId = parseNumericUserId(userKey);
-			}
-
-			if (userId != null)
-			{
-				statsByUserId.put(userId, stateEntry.getValue());
-			}
-			else  // only populate statsByUserName if user id is unknown, it's a legacy and will be removed later (hopefully)
-			{
-				statsByUserName.put(userName, stateEntry.getValue());
-			}
+			this.resources.setData(stateEntry.getKey(), stateEntry.getValue());
 		}
 	}
 
@@ -89,13 +75,7 @@ public class StateManager
 	{
 		if (GameState.isInGame(this.gameState.getState()))
 		{
-			UserDisplayData displayData = this.getInGameDisplayData(userIdString);
-			if (displayData.getInGame() != null)
-			{
-				displayData.getInGame().setMap(this.gameState.getMap());
-				displayData.getInGame().setSellout(this.gameState.getState() == GameState.INGAME_SELLOUT);
-			}
-			return displayData;
+			return this.getInGameDisplayData(userIdString);
 		}
 		else if (this.gameState.getState() == GameState.BROKEN)
 		{
@@ -107,15 +87,15 @@ public class StateManager
 		}
 		else if (this.gameState.getState() == GameState.STARTINGSTREAM)
 		{
-			return UserDisplayData.msg(null);
+			return UserDisplayData.empty();
 		}
 		else if (this.gameState.getState() == GameState.LEADERBOARDS)
 		{
-			return UserDisplayData.msg(null);
+			return UserDisplayData.empty();
 		}
 		else
 		{
-			return UserDisplayData.msg(null);
+			return UserDisplayData.empty();
 		}
 	}
 
@@ -127,50 +107,36 @@ public class StateManager
 			return UserDisplayData.msg("Please allow the extension to identify you");
 		}
 
-		UserDisplayData regularStats = statsByUserId.get(userId);
-		if (regularStats != null)
+		DataOrMessage<UserGameState> resourcesOrMessage = resources.getData(userId);
+		if (resourcesOrMessage.getData() != null) // *** validations have passed, userId is cached
 		{
-			return regularStats;
-		}
-
-		String userName = twitchApi.getUserDisplayName(userId);
-		if (userName == null)
-		{
-			return UserDisplayData.msg("Unable to fetch your userName, please contact admins");
+			UserDisplayData displayData = new UserDisplayData();
+			displayData.setState(resourcesOrMessage.getData());
+			displayData.setMap(this.gameState.getMap());
+			displayData.setSellout(this.gameState.getState() == GameState.INGAME_SELLOUT);
+			displayData.setEvents(events.getData(userId).getData()); // see *** above, fetching the data directly is safe
+			return displayData;
 		}
 		else
 		{
-			displayNameToUid.put(userName, userId); // save user name in the cache
-
-			UserDisplayData legacyStats = statsByUserName.get(userName); // do the lookup in legacy collection
-			if (legacyStats != null)
-			{
-				statsByUserId.put(userId, legacyStats); // save the data to the regular collection for faster lookup next time
-				return legacyStats;
-
-			}
-			else // no data found for the player whatsoever, but we know them, so they're ready to join
-			{
-				return UserDisplayData.msg("Type !play to join the game");
-			}
-		}
-	}
-
-	private static Integer parseNumericUserId(String str)
-	{
-		try
-		{
-			return Integer.parseInt(str);
-		}
-		catch (NumberFormatException e)
-		{
-			LOG.warn("Could not parse neither user id nor username from the key: " + str);
-			return null;
+			return UserDisplayData.msg(resourcesOrMessage.getMessage());
 		}
 	}
 
 	public GameStateContainer getCurrentState()
 	{
 		return this.gameState;
+	}
+
+	public void pushEvents(Map<String, List<String>> eventsBySomeUserIdentifiers)
+	{
+		eventsBySomeUserIdentifiers.forEach((key, value) ->
+		{
+			// simply add all events to the ones we already have
+			events.mergeData(key, value, (l1, l2) -> {
+				l1.addAll(l2);
+				return l1;
+			});
+		});
 	}
 }
---
+++ src/main/java/org/camokatuk/extensionserver/UserGameState.java
@@ -0,0 +1,162 @@
+package org.camokatuk.extensionserver;
+
+import java.util.List;
+
+public class UserGameState
+{
+	private int gas;
+	private int minerals;
+	private int terrazine;
+	private int gasIncome;
+	private int mineralsIncome;
+	private int gasTax;
+	private int mineralsTax;
+	private int stance;
+	private String supply;
+	private String feeding;
+	private Workers workers;
+
+	public int getStance()
+	{
+		return stance;
+	}
+
+	public void setStance(int stance)
+	{
+		this.stance = stance;
+	}
+
+	public int getTerrazine()
+	{
+		return terrazine;
+	}
+
+	public void setTerrazine(int terrazine)
+	{
+		this.terrazine = terrazine;
+	}
+
+	public int getGas()
+	{
+		return gas;
+	}
+
+	public void setGas(int gas)
+	{
+		this.gas = gas;
+	}
+
+	public int getMinerals()
+	{
+		return minerals;
+	}
+
+	public void setMinerals(int minerals)
+	{
+		this.minerals = minerals;
+	}
+
+	public String getFeeding()
+	{
+		return feeding;
+	}
+
+	public void setFeeding(String feeding)
+	{
+		this.feeding = feeding;
+	}
+
+	public int getGasIncome()
+	{
+		return gasIncome;
+	}
+
+	public void setGasIncome(int gasIncome)
+	{
+		this.gasIncome = gasIncome;
+	}
+
+	public int getMineralsIncome()
+	{
+		return mineralsIncome;
+	}
+
+	public void setMineralsIncome(int mineralsIncome)
+	{
+		this.mineralsIncome = mineralsIncome;
+	}
+
+	public int getGasTax() {
+		return gasTax;
+	}
+
+	public void setGasTax(int gasTax) {
+		this.gasTax = gasTax;
+	}
+
+	public int getMineralsTax() {
+		return mineralsTax;
+	}
+
+	public void setMineralsTax(int mineralsTax) {
+		this.mineralsTax = mineralsTax;
+	}
+
+	public String getSupply()
+	{
+		return supply;
+	}
+
+	public void setSupply(String supply)
+	{
+		this.supply = supply;
+	}
+
+	public Workers getWorkers() {
+		return workers;
+	}
+
+	public void setWorkers(Workers workers) {
+		this.workers = workers;
+	}
+
+	public static class Workers
+	{
+		private int minerals;
+		private int gas;
+		private int idle;
+		private int moving;
+
+		public int getMinerals() {
+			return minerals;
+		}
+
+		public void setMinerals(int minerals) {
+			this.minerals = minerals;
+		}
+
+		public int getGas() {
+			return gas;
+		}
+
+		public void setGas(int gas) {
+			this.gas = gas;
+		}
+
+		public int getIdle() {
+			return idle;
+		}
+
+		public void setIdle(int idle) {
+			this.idle = idle;
+		}
+
+		public int getMoving() {
+			return moving;
+		}
+
+		public void setMoving(int moving) {
+			this.moving = moving;
+		}
+	}
+}
