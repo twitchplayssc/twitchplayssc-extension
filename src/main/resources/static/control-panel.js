555cc688c3ab729d370b902eb387b15ecc234853
camokatuk <black.valuable.notebook@gmail.com>
2021-10-17 04:58:45 +0200

    potentially working polls tab

--- src/main/resources/static/control-panel.js
+++ src/main/resources/static/control-panel.js
@@ -1,9 +1,10 @@
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 var pollIntervalId = null;
 var TABS = [
     {
         name: "Polls",
         fetchFn: fetchPolls,
-        fetchIntervalMs: 1000
+        fetchIntervalMs: 3000
     },{
         name: "Skills",
         fetchFn: fetchSkills,
@@ -29,6 +30,10 @@ var TABS = [
 var ACTIVE_TAB = TABS[0];
 
 $(document).ready(function() {
+    function stopFetchingAllData() {
+        if (pollIntervalId) clearInterval(pollIntervalId);
+    }
+
     for (var i = 0; i < TABS.length; i++) {
         var tabLink = $('<a/>').attr('id', "tab" + i).addClass('inactive').text(TABS[i].name);
         var tab = $('<span/>').addClass("tab").append(tabLink);
@@ -42,29 +47,33 @@ $(document).ready(function() {
             $("#tab" + this.name).fadeIn('slow');
         };
         TABS[i].open = function() {
+            stopFetchingAllData();
             this.activate();
-            console.log("opened " + this.name);
             this.focus();
         };
         TABS[i].focus = function() {
-            if (pollIntervalId) clearInterval(pollIntervalId);
-            pollIntervalId = setInterval(this.fetchFn, this.fetchIntervalMs)
+            if (this.fetchFn) {
+                this.fetchFn();
+                pollIntervalId = setInterval(this.fetchFn, this.fetchIntervalMs);
+            }
         };
     }
 
     $('.tab a').click(function(){
         var tabIndex = parseInt($(this).attr('id').substring(3));
-        if ($(this).hasClass('inactive')) { //this is the start of our condition
+        if ($(this).hasClass('inactive')) { // this is the start of our condition
             TABS[tabIndex].open();
         }
     });
 
     $('#control-panel-toggle').click(function() {
         $('#control-panel').toggle();
-        if (pollIntervalId) clearInterval(pollIntervalId);
-        if ($('#control-panel').is(":visible")) {
+        stopFetchingAllData();
+        var isOpen = $('#control-panel').is(":visible");
+        if (isOpen) {
             ACTIVE_TAB.focus();
         }
+        $(this).text(isOpen ? 'Close Control Panel' : 'Open Control Panel');
     });
 
     ACTIVE_TAB.activate();
@@ -72,6 +81,144 @@ $(document).ready(function() {
 
 function fetchPolls() {
     console.log("Imma fetch some Polls");
+    updatePolls($('#tabPolls'), DEBUG_POLLS());
+
+    ebsReq({
+    	url: OVERLAY_API_BASE_URL + '/polls',
+    	type: 'GET',
+    	success: function(data) {
+       	    updatePolls($('#tabPolls'), data);
+        },
+        error: function(e) {
+            // $('#tabPolls').text(e.status + ' - ' + e.statusText)
+        }
+    });
+}
+
+function updatePolls(tab, data) {
+    if (!data.polls) return;
+    var thisIter = Math.random() + '';
+    for (var i = 0; i < data.polls.length; i++) {
+        var poll = data.polls[i];
+        var pollContainerId = 'poll-container-' + poll.id;
+        var pollContainer = $('#' + pollContainerId);
+
+        if (pollContainer.length == 0)
+        {
+            pollContainer = $('<div/>').attr('id', pollContainerId).addClass("poll-container");
+            var pollName = $('<span/>').addClass("pollName").text(poll.name);
+            if (poll.disabled) {
+                pollName.append($('<span class="pollError"/>').text(' - ' + poll.disabled));
+            }
+            if (poll.votesLeft) {
+                pollName.append($('<span class="pollInfo"/>').text(' - ' + poll.votesLeft + ' votes left'));
+            }
+            var pollTable = $('<table/>');
+            for (var j = 0; j < poll.options.length; j++) {
+                var option = poll.options[j];
+                var optionTr = $('<tr/>');
+                var optionValue = $('<span/>').addClass("currentValue");
+                optionValue.css('width', 100 * option.progress + "%").text(option.name);
+                optionTr.append($('<td/>').append(optionValue));
+                var submitEl = createOptionSubmitElements(poll, j)
+                optionTr.append(submitEl);
+                pollTable.append(optionTr);
+            }
+            pollContainer.append(pollName).append(pollTable);
+            tab.append(pollContainer);
+        }
+        else // poll already rendered, just need to update results
+        {
+            var pollTable = pollContainer.find('table');
+            for (var j = 0; j < poll.options.length; j++) {
+                var option = poll.options[j];
+                var optionEl = pollTable.children(":nth-child(" + (j + 1) + ")");
+                optionEl.find(".currentValue").animate({
+                    width: 100 * option.progress + "%"
+                }, 1000, function() {});
+
+            }
+        }
+        pollContainer.attr('iterId', thisIter);
+    }
+
+    var maxTerra = data.availableTerrazine ? data.availableTerrazine : Number.MAX_SAFE_INTEGER;
+    $("input.terra").attr('max', maxTerra).change();
+    $(".poll-container[iterId!='" + thisIter + "']").fadeOut("slow");
+}
+
+function createOptionSubmitElements(poll, optionIndex) {
+    var parentEl = $("<td/>")
+    if (poll.voteType == 'Terrazine') {
+        var terraInput = $("<input class='terra' type='number' step='100' min='0' value='0'/>");
+        terraInput.clamp();
+        terraInput.keypress(function(e) {
+            if (e.which == 13) {
+                pollSubmit($(this), poll.id, optionIndex);
+            }
+        });
+
+        parentEl.append(terraInput);
+        var scrollers = $("<div class='scrollers'/>");
+        var numberup = $("<div class='numberup'/>").click(function(e) {
+            if (e.shiftKey) terraInput[0].value = terraInput[0].max;
+            else terraInput[0].stepUp();
+        });
+        var numberdown = $("<div class='numberdown'/>").click(function(e) {
+            terraInput[0].stepDown();
+        });
+        parentEl.append(scrollers.append(numberup).append(numberdown));
+    }
+    var submitBtn = $("<input class='submitBtn' type='button' value='Submit'/>");
+    var disabledText = poll.disabled || poll.options[optionIndex].disabled;
+    submitBtn.toggleClass('disabled', disabledText !== undefined);
+    submitBtn.attr('title', disabledText);
+    submitBtn.click(function () {
+        pollSubmit($(this), poll.id, optionIndex);
+    });
+    parentEl.append(submitBtn);
+    return parentEl;
+}
+
+(function( $ ){
+    $.fn.clamp = function() {
+        function doet() {
+            var myMin = parseInt($(this).attr('min'));
+            var myMax = parseInt($(this).attr('max'));
+            $(this).val(clamp(myMin, $(this).val(), myMax));
+        }
+        $(this).change(doet);
+        return this;
+    };
+})( jQuery );
+
+function pollSubmit(element, pollId, optionIndex) {
+    var terraEl = element.parent().find('.terra');
+    if (terraEl && terraEl.val() <= 0) {
+        return;
+    }
+
+    ebsReq({
+    	url: OVERLAY_API_BASE_URL + '/polls/vote',
+    	data: {
+    	    pollId : pollId,
+    	    optionIndex : optionIndex,
+    	    terrazine: terraEl ? terraEl.val() : null,
+    	    timestamp: new Date().getTime()
+    	},
+    	type: 'POST',
+    	success: function(data) {
+    	    terraEl.val(0);
+       	    updatePolls($('#tabPolls'), data);
+       	    console.log(element);
+                console.log("Cool, you voted with " + terraEl.val());
+        },
+        error: function(data) {
+               	    updatePolls($('#tabPolls'), data);
+               	    console.log(element);
+                        console.log("ERROR, you voted with " + terraEl.val());
+                }
+    });
 }
 
 function fetchHelp() {
