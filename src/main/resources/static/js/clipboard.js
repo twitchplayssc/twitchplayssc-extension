1b4af69c1d87f0b007f8849e43561e475be4352e
camokatuk <black.valuable.notebook@gmail.com>
2022-11-01 21:10:38 +0100

    fixed clipboard + fixed minimap position + minor js refactoring

---
+++ src/main/resources/static/js/clipboard.js
@@ -0,0 +1,55 @@
+let $clipboard = $("<input id='clipboard'>").appendTo('body').righteousToggle(false);
+let CLIPBOARD_ATTR_T = 't'; // command text
+let CLIPBOARD_ATTR_C = 'c'; // coords, meaning minimap
+let CLIPBOARD_ATTR_M = 'm';  // multiplier
+let CLIPBOARD_TARGET_C = { attr: 'c' };  // minimap target
+
+// key - any object of type CLIPBOARD_BUFFER_PART (including ones coming from the server)
+// the ones coming from the server will have attr=t and one of these combos: (tcm, tc, tm, t)
+function copyToClipboard(text, key) {
+    var oldValue = $clipboard.attr(key.attr);
+    $clipboard.attr(key.attr, text);
+
+    if (key.combo) {
+        $clipboard.attr("combo", JSON.stringify(key.combo));
+    }
+
+    var combo = JSON.parse($clipboard.attr("combo"));
+
+    var newVal;
+    var shouldForgetCombo = key == CLIPBOARD_TARGET_C && !combo.includes(CLIPBOARD_ATTR_C);
+    if (!combo || shouldForgetCombo) {
+        forgetCombo();
+        newVal = text;
+    } else {
+        // if combo allows multiplier, increment it
+        if (combo.includes(CLIPBOARD_ATTR_M) && oldValue == text) {
+            var multiplierText = $clipboard.attr(CLIPBOARD_ATTR_M);
+            var multiplier = multiplierText ? parseInt(multiplierText) : 1;
+            $clipboard.attr(CLIPBOARD_ATTR_M, multiplier + 1);
+        }
+
+        newVal = combo.map(a => $clipboard.attr(a)).join(" ").replace(/\s+/g, ' ');
+    }
+
+    // clipboard stuff and periodic clear
+
+    if (key.timeoutHandle) clearTimeout(key.timeoutHandle);
+    key.timeoutHandle = setTimeout(forgetCombo, 3000); // keep last timeout handle for each type of token
+
+    $clipboard.righteousToggle(true).val(newVal.trim()).select();
+    document.execCommand("copy");
+    $clipboard.righteousToggle(false);
+}
+
+function forgetCombo() {
+    $clipboard.attr(CLIPBOARD_ATTR_T, null);
+    $clipboard.attr(CLIPBOARD_ATTR_C, null);
+    $clipboard.attr(CLIPBOARD_ATTR_M, null);
+    $clipboard.attr("combo", null);
+}
+
+$(function () {
+	$('.minimap').trackClicks($('.minimap-click-data'), crds => ((crds.x + 1) + " " + crds.y),
+	    crds => copyToClipboard("(" + (crds.x + 1) + " " + crds.y + ")", CLIPBOARD_TARGET_C), 100, 100);
+});
---
+++ src/main/resources/static/js/utils.js
@@ -0,0 +1,110 @@
+$.fn.extend({
+	righteousToggle: function(show) { // avoids triggering show animation when not necessary
+		if ($(this).is(':visible') && !show) {
+			$(this).hide();
+		} else if ($(this).is(':hidden') && show) {
+			$(this).show();
+		}
+		return this;
+	},
+	numberChange: async function(newNumber, str, onstep) {
+	    if (!document.hidden ) {
+            $(this).prop('Counter', $(this).text()).stop().animate({ Counter: newNumber }, {
+                duration: 800,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text((str ? str : '') + Math.ceil(now));
+                    if (onstep) onstep($(this), Math.ceil(now));
+                }
+            });
+        }
+        return this;
+    },
+	rgbColor: function(r, g, b) {
+		return $(this).css("color", "rgb(" + r + "," + g + "," + b + ")");
+	},
+	whiteColor: function() {
+	    return $(this).rgbColor(255, 255, 255);
+	},
+	taxColor: function(tax) {
+	    if (!tax)
+	    {
+	        return $(this).whiteColor();
+	    }
+	    var r = 255;
+        var g = 255 - Math.max(5 * tax - 245, 0);
+        var b = 255 - Math.min(5 * tax, 255);
+        return $(this).rgbColor(r, g, b);
+	},
+	mouseCoords(event, scaleWidth, scaleHeight) {
+        var posX = event.pageX - $(this).position().left;
+        var posY = event.pageY - $(this).position().top;
+
+        var myX = Math.floor(posX * scaleWidth / $(this).width());
+        var myY = Math.floor(scaleHeight - posY * scaleHeight / $(this).height());
+        return {x: myX, y: myY};
+    },
+	trackClicks: function(valueHolder, valueDisplayFn, valueClipFn, scaleWidth, scaleHeight) {
+	    function myClickTrack(event) {
+	        if (event.which == 1) {
+                var crds = $(this).mouseCoords(event, scaleWidth, scaleHeight);
+                valueClipFn(crds);
+            }
+	    }
+
+	    $(this).mouseout(function(event) {
+	        valueHolder.text('');
+	    }).mousemove(function(event) {
+	        var crds = $(this).mouseCoords(event, scaleWidth, scaleHeight);
+	        var str = valueDisplayFn(crds);
+	        valueHolder.text(str);
+	    }).unbind('click').bind('click', myClickTrack);
+	},
+	percentWidth: function() {
+	    return 100 * $(this).width() / $(window).width();
+	},
+	percentHeight: function() {
+        return 100 * $(this).height() / $(window).height();
+    },
+    percentLeft: function() {
+        return 100 * $(this).position().left / $(window).width();
+    },
+    percentTop: function() {
+        return 100 * $(this).position().top / $(window).height();
+    },
+    percentValue: function(prop, newVal) {
+         return $(this).css(prop, newVal + '%');
+    },
+	scaleToRatio: function(ratioW2H) { // preserves the center
+	    var originalPosition;
+        var originalDimensions;
+	    if (!$(this).attr('originalPosition')) { // store the original values
+	        originalPosition = {left: $(this).percentLeft(), top: $(this).percentTop()};
+	        originalDimensions = {width: $(this).percentWidth(), height: $(this).percentHeight()};
+	        $(this).attr('originalPosition', JSON.stringify(originalPosition));
+	        $(this).attr('originalDimensions', JSON.stringify(originalDimensions));
+	    } else { // reset to original dimensions first
+	         originalPosition = JSON.parse($(this).attr('originalPosition'));
+             originalDimensions = JSON.parse($(this).attr('originalDimensions'));
+             $(this).percentValue('left', originalPosition.left);
+             $(this).percentValue('top', originalPosition.top);
+             $(this).percentValue('width', originalDimensions.width);
+             $(this).percentValue('height', originalDimensions.height);
+	    }
+
+	    if (ratioW2H > 1) { // apply the ratio thingy
+            let originalHeight = originalDimensions.height;
+	        let newHeight = originalHeight / ratioW2H;
+	        $(this).percentValue('top', originalPosition.top + ((originalHeight - newHeight) / 2));
+	        $(this).percentValue('height', newHeight);
+	    } else if (ratioW2H < 1) {
+	        let originalWidth = originalDimensions.width;
+	        let newWidth = originalWidth * ratioW2H;
+	        $(this).percentValue('left', originalPosition.left + ((originalWidth - newWidth) / 2));
+	        $(this).percentValue('width', newWidth);
+	    }
+    },
+    tooltip: function(txt) {
+        $(this).addClass('tooltip').append('<span class="tooltiptext">' + txt + '</span>');
+    }
+});
